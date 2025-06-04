d714362f9a3f1d455bc9435c1ba3093317567282
hkrTcodes <fi1er01hkr10ty@gmail.com>
2025-06-04 20:08:06 +0900

    [fix]: content.config.tsの型明示

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,9 +1,11 @@
 import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
+const blogLoader = glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' });
+
 const blog = defineCollection({
 	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+	loader: blogLoader,
 	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
